39d3658935d7b0e3fd99af452883e30b35c1d2ad
darwishmammo <34026849+darwishmammo@users.noreply.github.com>
2019-08-30 13:07:52 +0200

    initial build of logger client

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import Navbar from "./components/Navbar";
+import Layout from "./components/Layout";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
